9596f593fb88eb889148b2bd8a45fd4355e497d3
HackHusky <kurst811@gmail.com>
2017-02-08 22:23:14 -0800

    Login/Signup finish

--- blogpost/app/controllers/application_controller.rb
+++ blogpost/app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authorize
+  protected
+  def authorize
+ 	#unless User.find_by(id: session[:user_id])
+ 		#redirect_to login_url, notice: "You thought you could gain unauthorised access? LOL"
+ 	#end
+  end
+
   def home
   end	
 
@@ -8,5 +17,5 @@ class ApplicationController < ActionController::Base
   def demo
   	#ruby code here
   end
-  
+
 end
--- blogpost/config/routes.rb
+++ blogpost/config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
+  #get 'admin/index'
+  #get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
+
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   get 'home', to: 'application#home'
   get 'test', to: 'application#test'
   get 'demo', to: 'application#demo'
   get 'users/new', to: 'users#new'
   root 'application#home'
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
